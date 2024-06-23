d2fc0546aa9c8935400267a8d832ccc45815627c
ihgazi <irfanhgazi@gmail.com>
2024-06-23 12:18:19 +0530

    UPDATE: Created UserInfo type - Added proxy in next config to bypass CORS check

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    // Using proxy to prevent CORS issues
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8800"}/:path*`,
+            }
+        ]
+    }
+};
 
 export default nextConfig;
